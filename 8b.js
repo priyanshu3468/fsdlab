e8941bc246dfaf66278d850a8dc6aeb1a56d641d
pp <you@example.com>
2024-12-18 22:07:07 +0530

    8b

---
+++ 8b.js
@@ -0,0 +1,21 @@
+const EventEmitter = require('events');
+
+// Create an instance of EventEmitter
+const eventEmitter = new EventEmitter();
+
+// Define an event listener function
+const greetListener = (name) => {
+  console.log(`Hello, ${name}!`);
+};
+
+// Add the event listener for the 'greet' event
+eventEmitter.on('greet', greetListener);
+
+// Emit the 'greet' event
+eventEmitter.emit('greet', 'Priyanshu'); // Output: Hello, Aditya!
+
+// Remove the event listener for the 'greet' event
+eventEmitter.removeListener('greet', greetListener);
+
+// Emit the 'greet' event again (no output since listener is removed)
+eventEmitter.emit('greet','Priyanshu');
